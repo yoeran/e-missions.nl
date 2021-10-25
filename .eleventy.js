c2e797ea691e41082529b89105b9d06b82a4fc2f
Yoeran <contact@yoeran.nl>
2021-10-25 16:14:26 +0200

    Upgrade packages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-const pluginSvgSprite = require("eleventy-plugin-svg-sprite");
 const markdownIt = require("markdown-it");
 
 const filters = require("./utils/filters.js");
@@ -12,10 +11,6 @@ module.exports = function (config) {
   // Plugins
   config.addPlugin(pluginRss);
   config.addPlugin(pluginNavigation);
-  config.addPlugin(pluginSvgSprite, {
-    path: "./src/assets/icons",
-    svgSpriteShortcode: "iconsprite",
-  });
 
   // Filters
   Object.keys(filters).forEach((filterName) => {
